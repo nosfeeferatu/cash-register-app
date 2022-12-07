e0c995052b1d27860977554f6d46e8ccc8198528
Naseef Mashood <nosfeeferatu@gmail.com>
2022-12-07 14:11:23 +0530

    feat: displays next section only with button click

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 let billAmount = document.querySelector("#bill-amount");
 let cashGiven = document.querySelector("#cash-given");
 let calculate = document.querySelector("#calculate");
+let nextButton = document.querySelector("#next-button");
+let nextSection = document.querySelector(".next-section");
+let table = document.querySelector("table");
 let errorMessage = document.querySelector("#error-message");
 let showOnTable = document.querySelectorAll(".no-of-notes");
 
 let denominations = [2000,500,100,20,10,5,1];
 
-calculate.addEventListener("click", ()=> {
-    errorMessage.style.display = "none";
+nextButton.addEventListener("click", ()=> {
+    hideElement(errorMessage);
+    hideElement(nextSection);
     if(billAmount.value > 0) {
-        if(cashGiven.value >= billAmount.value) {
-            let changeMoney  = cashGiven.value - billAmount.value;
-            calculateChange(changeMoney); 
-        }
-        else{
-            showError("Where's the rest of the money?");
-        }
+        nextSection.style.display = "flex";
+        // Calculate Button Clicking
+        calculate.addEventListener("click", () => {
+            hideElement(errorMessage);
+            hideElement(table);
+            if(cashGiven.value >= billAmount.value) {
+                table.style.display = "table";
+                let changeMoney  = cashGiven.value - billAmount.value;
+                calculateChange(changeMoney); 
+            }
+            else{
+                showError("Where's the rest of the money?");
+            }
+        })
     }
     else{
         showError("Bill amount is invalid");
     }
 })
+
+function hideElement(elementToHide){
+    elementToHide.style.display = "none";
+}
 
 function calculateChange(changeMoney) {
     for(let i=0; i<denominations.length; i++) {
